b0ca30f0919e4e7c9015bbcbd5db06cd17051834
andybelucci <beluccianderson@gmail.com>
2026-04-20 12:30:17 -0300

    feat: Writable Signals for Search State

--- src/app/products/products-grid/products-grid.ts
+++ src/app/products/products-grid/products-grid.ts
@@ -1,15 +1,19 @@
-﻿import { Component, signal } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { ProductCard } from '../product-card/product-card';
 import { Product } from '../product';
-import { MatIconModule } from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import { MatIcon } from '@angular/material/icon';
+import {FormsModule} from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-products-grid',
-  imports: [ProductCard, MatIconModule],
+  imports: [ProductCard, MatIcon, MatInputModule, FormsModule, MatFormFieldModule],
   templateUrl: './products-grid.html',
-  styleUrl: './products-grid.scss',
+  styleUrls: ['./products-grid.scss'],
 })
 export class ProductsGrid {
+  protected readonly searchTerm = signal('');
   protected readonly products = signal<Product[]>([
     {
       id: 1,
@@ -32,4 +36,12 @@ export class ProductsGrid {
       originalPrice: 1199.99,
     },
   ]);
+
+  protected clearSearch(): void {
+    this.searchTerm.set('');
+  }
+
+  protected trimSearchTerm(): void {
+    this.searchTerm.update((value) => value.trim());
+  }
 }
